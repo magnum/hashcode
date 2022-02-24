3e437cdd61305929dfbda4a7f0c716d09d433cdf
Antonio Molinari <molinari@incode.it>
2022-02-24 16:43:21 +0100

    wip

--- parser.rb
+++ parser.rb
@@ -1,4 +1,6 @@
+
 class Parser
+   attr :simulation 
 
    SEPARATOR = " "
 
@@ -11,13 +13,14 @@ class Parser
       puts "loading file #{input_file_path}"
       File.open(input_file_path).read.each_line.with_index do |line, index|
          @data << line.strip
+         values = line.split " "
          #todo
+         puts line
       end
 
       output_file_path = input_file_path.gsub File.extname(input_file_path), "_submitted_#{Time.now.to_i}#{File.extname(input_file_path)}"
       lines = @data.join("\n")
-      File.write(File.join("./output/", output_file_path), lines)
-
+      #File.write(File.join("./output/", output_file_path), lines)
    end
 
 end
